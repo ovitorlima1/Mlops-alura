521dd3e876f9a9bf906869ce346c29b7bcb8c805
ovitorlima1 <vitor.matheus@tpfe.com.br>
2025-09-01 22:47:58 -0300

    :construction: Contruindo API

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from textblob import TextBlob
 from deep_translator import GoogleTranslator
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+df = pd.read_csv('casas.csv')
+
+colunas = ['tamanho', 'ano', 'garagem']
+
+X = df.drop('preco', axis=1)
+y = df['preco']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42)
+
+modelo = LinearRegression()
+modelo.fit(X_train, y_train)
+
 
 app = Flask(__name__)
 
@@ -17,6 +34,14 @@ def sentimento(frase):
     polaridade = traducao_blob.sentiment.polarity
 
     return f'Polairdade: {polaridade}'
+
+@app.route('/cotacao/', methods=['POST'])
+def cotacao():
+    dados = request.get_json()
+    dados_input = [dados[col] for col in colunas]
+    preco_estimado = modelo.predict([dados_input])
+
+    return jsonify(preco_estimado=preco_estimado[0])
 
 
 app.run(debug=True)
